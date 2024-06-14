dfd20775cae86148ab6978612e4c8c62596f8c8f
Kevin <kevingomes793@gmail.com>
2024-06-14 17:46:09 -0300

    pRIMEIRO COMMIT
    Signed-off-by: Kevin <kevingomes793@gmail.com>

---
+++ script/script.js
@@ -0,0 +1,59 @@
+ //Inicializar variaveis
+let contMonth = 0 
+// Capturar valor do select (mês escolhido)
+let tagSelect = document.getElementById('selectMes')
+let resultado = document.getElementById('result')
+
+//
+//Captuarar menor lance
+let inputLowest = document.getElementById('lowestTxt')
+//Capturar menor lance
+let inputHighest = document.getElementById('highestTxt')
+
+//Verifica se está vazio
+function isInvalid(){
+    if(inputLowest.value <= 0 || inputHighest.value <= 0){
+        return true
+        //alert('Pelo menos um dos campos dos lances estão vazios. Preencha com a porcentagem dos lances.')
+    }else{
+        return false
+    }
+}
+//Função para verificar se número é maior do que 100%
+function verificaMaiorCem(){
+    if(Number(inputLowest.value) > 100 || Number(inputLowest.value) > 100){
+        return true
+    }else{
+        return false
+    }
+
+
+}
+//Função calcular
+function calcular(){
+    if((isInvalid(inputLowest) || isInvalid(inputHighest)) || verificaMaiorCem(inputLowest,inputHighest)){
+        alert('Pelo menos um dos campos dos lances estão vazios ou o valor é maior do que 100%. Preencha a porcentagem dos lances corretamente.')
+    }else{
+        let lowest = Number(inputLowest.value);
+        let highest = Number(inputHighest.value);
+        
+        let soma = lowest + highest
+        let media = soma / 2
+        
+        resultado.innerHTML = `Lance Médio: <strong>${media}%</string>`
+    }
+    /*}
+    //Verifica se está vazio
+    /*if(inputLowest.value == 0 || inputHighest.value == 0){
+        alert('Pelo menos um dos campos dos lances estão vazios. Preencha com a porcentagem dos lances.')
+    }else if(){
+
+    }/*else{
+        //A conversao deve ser feita dentro da função
+        
+        
+
+    }*/
+    
+}
+
